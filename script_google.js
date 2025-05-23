704b119fd41e8ea73bdac8ed375247cd6117c137
Raúl Andrés Sira Alvarez <46427497+rasa0873@users.noreply.github.com>
2025-05-23 16:43:25 -0400

    Update script_google.js
    Adding console instructions, for tracing

--- script_google.js
+++ script_google.js
@@ -68,6 +68,7 @@ async function submitEvaluation() {
     `;
 
     const backendURL = "https://us-central1-rasa-agent-3-badg.cloudfunctions.net/evaluate_google";
+    console.log("url for evaluate_google function, built");
 
     try {
         const response = await fetch(backendURL, {
